9576c37ea8bcb7147620123818df9a6d69891fc5
kguard14 <kuzog2026@gmail.com>
2026-09-16 06:29:11 -0400

    Rename model artifact to model_export

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def go(config: DictConfig):
                     "stratify_by": config["modeling"]["stratify_by"],
                     "rf_config": rf_config,
                     "max_tfidf_features": config["modeling"]["max_tfidf_features"],
-                    "output_artifact": "random_forest_export",
+                    "output_artifact": "model_export",
                 },
             )
 
@@ -122,7 +122,7 @@ def go(config: DictConfig):
                 "main",
                 env_manager="conda",
                 parameters={
-                    "mlflow_model": "random_forest_export:prod",
+                    "mlflow_model": "model_export:prod",
                     "test_dataset": "test_data.csv:latest",
                 },
             )
